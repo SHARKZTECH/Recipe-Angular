9e6beb2141f3d87f96309462b0e455d7e3310b54
sharkz <mosenjemudogoz@gmail.com>
2024-06-05 21:44:30 +0300

    recipe-item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { HeaderComponent } from './components/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   title = 'recipe';
+
+  itemCount=6;
+  items=new Array(this.itemCount);
 
   ngOnInit(): void {
     if (typeof document !== 'undefined') {
