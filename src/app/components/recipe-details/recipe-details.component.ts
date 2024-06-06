087005936b18e42c110f782ccc5dd0864b9d762f
sharkz <mosenjemudogoz@gmail.com>
2024-06-06 15:08:35 +0300

    details

--- src/app/components/recipe-details/recipe-details.component.ts
+++ src/app/components/recipe-details/recipe-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../../services/recipe.service';
 
 @Component({
   selector: 'app-recipe-details',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './recipe-details.component.html',
   styleUrl: './recipe-details.component.css'
 })
-export class RecipeDetailsComponent {
+export class RecipeDetailsComponent implements OnInit {
+
+  meal:any;
+
+  constructor(private recipeService:RecipeService){}
+
+  ngOnInit(): void {
+    this.recipeService.getRecipeDetail("52885").subscribe(value=>{
+      let m=value.meals[0]
+      this.meal=m;
+      });
+    console.log(this.meal)
+  }
 
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -9,10 +9,16 @@ export class RecipeService {
 
   apiUrl="https://www.themealdb.com/api/json/v1/1/filter.php?i="
 
+  apiUrl_details="https://www.themealdb.com/api/json/v1/1/lookup.php?i="
+
 
     constructor(private http:HttpClient) { }
 
     getRecipes(search:any): Observable<any> {
-      return this.http.get(`${this.apiUrl}`+`${search}`)     
+      return this.http.get(`${this.apiUrl}`+`${search}`);    
     };
+
+    getRecipeDetail(id:any):Observable<any>{
+      return this.http.get(`${this.apiUrl_details}`+`${id}`);
+    }
 }
